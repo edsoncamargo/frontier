e29f779cdbfcffbd76a3cdde7a3c5abe58314c6b
Edson Camargo <dinhocmenezes@hotmail.com>
2024-08-19 21:17:29 -0300

    [IMP] Cronjob to request backend eact 5 minutes

--- server/src/services/jobs.service.ts
+++ server/src/services/jobs.service.ts
@@ -1,23 +1,23 @@
 import cron from 'node-cron';
-import { prisma } from '../lib/prisma';
+import { env } from '../env';
 
 const pingMongoAtlas = async () => {
   const startTime = Date.now();
 
-  await prisma.user.findMany();
+  await fetch(`${env.BACKEND_URL}/api/v1/public/vips`);
 
   const endTime = Date.now();
   const duration = endTime - startTime;
 
   console.log(
-    '\n>> [INFO] Ping enviado ao MongoDB Atlas para manter a conexão ativa.'
+    `\n>> [INFO] Ping enviado ao '${env.BACKEND_URL}/api/v1/public/vips' para manter a conexão ativa.`
       .blue
   );
   console.log(`>> [INFO] Tempo de execução: ${duration} ms.\n\n`.blue);
 };
 
 const scheduleCronJobs = () => {
-  cron.schedule('*/15 * * * *', pingMongoAtlas);
+  cron.schedule('*/5 * * * *', pingMongoAtlas);
 };
 
 export default scheduleCronJobs;
