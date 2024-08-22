8bf612251caf8078cb4138814c6fec12a436b698
Edson Camargo <dinhocmenezes@hotmail.com>
2024-08-22 16:53:10 -0300

    [wip] work in progress message

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -134,9 +134,8 @@ function App() {
 
         <section>
           <p className='text-2xl text-paragraph-50 w-full'>
-            Trabalho em progresso,
-            <span className='font-bold whitespace-nowra'>
-              {' '}
+            Trabalho em progresso,{' '}
+            <span className='font-bold whitespace-nowrap'>
               novidades em breve ⚠️
             </span>
           </p>
